765383f6ab12827968e30336aa579b6759112f5c
Verkhoturkin <verhoturkin@gmail.com>
2020-04-13 10:52:04 +0500

    initial commit

---
+++ src/main/java/com/example/aop/aspectexample/AspectExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.aop.aspectexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspectExampleApplication.class, args);
+	}
+
+}
