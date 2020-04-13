c0e0a1bd77c56d13f9409dcc45d11f08662d879b
Verkhoturkin <verhoturkin@gmail.com>
2020-04-13 12:47:45 +0500

    add poincut variations

--- src/main/java/com/example/aop/aspectexample/aspect/TimeMeasurementAspect.java
+++ src/main/java/com/example/aop/aspectexample/aspect/TimeMeasurementAspect.java
@@ -20,11 +20,33 @@ public class TimeMeasurementAspect {
         LOG.info(joinPoint.toShortString() + " took " + (System.currentTimeMillis() - start) + "ms");
     }
 
+    @Around("services()")
+    public void measureAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        joinPoint.proceed();
+        LOG.info(joinPoint.toShortString() + " service took " + (System.currentTimeMillis() - start));
+    }
+
+    @Around("packages()")
+    public void measurePackage(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        joinPoint.proceed();
+        LOG.info(joinPoint.toShortString() + " from package services took " + (System.currentTimeMillis() - start));
+    }
+
     @Pointcut("execution(* com.example.aop.aspectexample.service.FirstService.doSomething())")
     public void firstService() {
     }
 
     @Pointcut("execution(* com.example.aop.aspectexample.service.SecondService.doSomething())")
     public void secondService() {
+    }
+
+    @Pointcut("execution(* (@org.springframework.stereotype.Service *).*(..))")
+    public void services() {
+    }
+
+    @Pointcut("within(com.example.aop.aspectexample.service.*)")
+    public void packages() {
     }
 }
