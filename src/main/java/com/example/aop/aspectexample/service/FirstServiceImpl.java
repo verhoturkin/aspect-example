9de828055ff60aec3fb09bac3a19a2c54df69e8e
Verkhoturkin <verhoturkin@gmail.com>
2020-04-13 11:25:57 +0500

    from sout to logging

--- src/main/java/com/example/aop/aspectexample/service/FirstServiceImpl.java
+++ src/main/java/com/example/aop/aspectexample/service/FirstServiceImpl.java
@@ -1,14 +1,20 @@
 package com.example.aop.aspectexample.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FirstServiceImpl implements FirstService {
+    private static final Logger LOG = LoggerFactory.getLogger("FirstServiceImpl");
+
     public void doSomething() {
         //some logic
         try {
-            System.out.println("Inside first service");
+            long start = System.currentTimeMillis();
+            LOG.info("Inside first service");
             Thread.sleep(500);
+            LOG.info("First service execution time: " + (System.currentTimeMillis() - start));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/main/java/com/example/aop/aspectexample/service/SecondServiceImpl.java
+++ src/main/java/com/example/aop/aspectexample/service/SecondServiceImpl.java
@@ -1,14 +1,20 @@
 package com.example.aop.aspectexample.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SecondServiceImpl implements SecondService {
+    private static final Logger LOG = LoggerFactory.getLogger("SecondServiceImpl");
+
     public void doSomething() {
         //some logic
         try {
-            System.out.println("Inside second service");
+            long start = System.currentTimeMillis();
+            LOG.info("Inside second service");
             Thread.sleep(200);
+            LOG.info("Second service execution time: " + (System.currentTimeMillis() - start));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
